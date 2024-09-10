5a16ed054d40996aa489bf8cf192684f4c0f449c
KostiantynKrasnostup <vladovskiy462@gmail.com>
2024-09-10 22:57:27 +0300

    Add files via upload

---
+++ Example3/Program.cs
@@ -0,0 +1,16 @@
+﻿string s = "біологія алгебра історія географія геометрія"; 
+string c = "і"; 
+string[] a;
+
+a = s.Split(' '); 
+
+for (int i = 0; i < a.Length; i++)
+{
+    int t = a[i].IndexOf(c); 
+    if (t != -1) 
+    {
+        Console.WriteLine(a[i]); 
+    }
+}
+
+Console.ReadKey(); 
